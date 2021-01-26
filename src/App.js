256665aa3d819c1f05a9c00e66b6f9e8bcd5a810
Jin Lin Cen <jinlincen@JinLins-MacBook-Pro.local>
2021-01-26 15:02:43 -0500

    name changed

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      title: 'Simon Cen',
+      title: 'Simon J. Cen',
       headerLinks: [ // navbar
         { title: 'Home', path: '/'},
         { title: 'About', path: '/about'},
