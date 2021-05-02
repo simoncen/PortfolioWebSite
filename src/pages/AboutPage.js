bce09a39bd3e98b75201d0a137c582a620c25d30
Jin Lin Cen <jinlincen@JinLins-MacBook-Pro.local>
2021-05-01 20:17:07 -0400

    aboutpage conflicts resolved

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -11,11 +11,7 @@ function AboutPage(props){
         <div>
             <Hero title={props.title}/>
             <Content>
-<<<<<<< HEAD
                 <p>Hi there, my name is Simon. I am a Computer Engineering student from the University of Toronto. I am a full-stack developer who have experience in ReactJs, Redux, MongoDB, ExpresJs, NodeJs, Bootstrap, and MaterialUI. </p>
-=======
-            <p>Hi there, my name is Simon. I am a Computer Engineering student from the University of Toronto. I am a full-stack developer who have experience in ReactJs, Redux, MongoDB, ExpresJs, NodeJs, Bootstrap, and MaterialUI. </p>
->>>>>>> da9d9f731079a085198448cb755471d3be826a71
                 <p>Coding is my passion. I write in many languages including Javascript, C++, Python, and Java. However, I am also very intrigued about doing bussiness and interacting with the stock market. </p>
                 <p>My vision is to convert the skills that I have to bring fascinating ideas with the most potential in improving our society to live.</p>
                 <p>I deeply believe in the idea of 'Knowledge reshapes destiny'.  This not only applies to myself, but also to the people I could give an impact to. Therefore, I hope that one day, I could become an entrepeneur and accomplish this goal!</p>
